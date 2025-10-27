f35cd597542319190ad9f6d9e8640859434277ee
H1r0t4ka <juanito_pachanga@hotmail.com>
2025-10-27 18:54:10 -0500

    OAuth de Google y se integra funcionalidad del formulario de créditos

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,12 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import auth from 'auth-astro';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  integrations: [auth()],
+  adapter: vercel()
 });
